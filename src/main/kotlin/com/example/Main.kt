87ac121bcf92d080a29c192bea02d3c71984072a
Scott Pierce <871169+ScottPierce@users.noreply.github.com>
2023-02-25 10:18:12 -0500

    Update Main.kt

--- src/main/kotlin/com/example/Main.kt
+++ src/main/kotlin/com/example/Main.kt
@@ -4,21 +4,12 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import org.flywaydb.core.Flyway
 import java.util.*
+import javax.sql.DataSource
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.minutes
 
 fun main() {
-    val dataSource = HikariDataSource(
-        HikariConfig().apply {
-            jdbcUrl = "jdbc:postgresql://localhost:8090/"
-            connectionTimeout = 10_000
-            dataSourceProperties = Properties().apply {
-                setProperty("user", "postgres")
-                setProperty("password", "test")
-
-                setProperty("dataSourceClassName", "org.postgresql.ds.PGSimpleDataSource")
-                setProperty("sslmode", "disable")
-            }
-        }
-    )
+    val dataSource = createDataSource()
 
     val flyway = Flyway
         .configure()
@@ -27,4 +18,33 @@ fun main() {
         .load()
 
     flyway.migrate()
+}
+
+private fun createDataSource(): DataSource {
+    val start = System.currentTimeMillis()
+
+    while (true) {
+        try {
+            return HikariDataSource(
+                HikariConfig().apply {
+                    jdbcUrl = "jdbc:postgresql://localhost:8090/"
+                    connectionTimeout = 10_000
+                    dataSourceProperties = Properties().apply {
+                        setProperty("user", "postgres")
+                        setProperty("password", "test")
+
+                        setProperty("dataSourceClassName", "org.postgresql.ds.PGSimpleDataSource")
+                        setProperty("sslmode", "disable")
+                    }
+                }
+            )
+        } catch (t: Throwable) {
+            val isTimeout = (System.currentTimeMillis() - start).milliseconds > 1.minutes
+            if (isTimeout) {
+                throw t
+            } else {
+                println("Retry...")
+            }
+        }
+    }
 }
